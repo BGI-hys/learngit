dc2c4db1fcb255da26b7b9ae9db85d05fc33db4e
huangyushan <huangyushan@genomics.cn>
2018-10-17 10:44:47 +0800

    add annotation

--- work.sh
+++ work.sh
@@ -9,17 +9,20 @@
 sudo apt-get install git
 
 #set your name and email
+#表示你这台机器上所有的Git仓库都会使用这个配置
+#当然也可以对某个仓库指定不同的用户名和Email地址
 git config --global user.name "Your Name"
 git config --global user.email "email@example.com"
 
-#mkdir repository
+#mkdir repository创建版本库
 mkdir learngit
 cd learngit
 
-#Initialize Git repository
+#Initialize Git repository初始化版本库
 git init 
 
 #add file to Git repository
+#添加文件到Git仓库，分两步：
 git add <file> #注意，可反复多次使用，添加多个文件;
 git commit -m <massage> #complete
 
@@ -33,3 +36,5 @@ git diff
 git log
 #如果嫌输出信息太多，看得眼花缭乱的，可以试试加上--pretty=oneline参数
 git log --pretty=oneline
+
+
