9e1d4e013fc8b99fcefdc45d366c21d063edbbb8
huangyushan <huangyushan@genomics.cn>
2018-10-17 10:23:43 +0800

    how to use git add and git commit

--- work.sh
+++ work.sh
@@ -11,3 +11,14 @@ sudo apt-get install git
 #set your name and email
 git config --global user.name "Your Name"
 git config --global user.email "email@example.com"
+
+#mkdir repository
+mkdir learngit
+cd learngit
+
+#Initialize Git repository
+git init 
+
+#add file to Git repository
+git add <file> #注意，可反复多次使用，添加多个文件;
+git commit -m <massage> #complete
