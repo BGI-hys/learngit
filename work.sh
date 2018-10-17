53af8ed5f8c8670687628e67ae8b7b8ac0c64b6e
huangyushan <huangyushan@genomics.cn>
2018-10-17 10:33:40 +0800

    git status and git diff

--- work.sh
+++ work.sh
@@ -22,3 +22,10 @@ git init
 #add file to Git repository
 git add <file> #注意，可反复多次使用，添加多个文件;
 git commit -m <massage> #complete
+
+#随时掌握工作区的状态
+git status
+
+#如果git status告诉你有文件被修改过，用git diff可以查看修改内容。
+git diff
+
