9d5ea6cad84401cc0801dfb7402f7a3599902f36
huangyushan <huangyushan@genomics.cn>
2018-10-17 09:59:28 +0800

    wrote a work run script

---
+++ work.sh
@@ -0,0 +1,13 @@
+#*************************************************************************
+#! /bin/sh
+#$ -S /bin/sh
+# Author: huangyushan@genomics.cn
+# Created Time: 2018年10月17日 星期三 09时54分52秒
+#*************************************************************************
+
+#install Git
+sudo apt-get install git
+
+#set your name and email
+git config --global user.name "Your Name"
+git config --global user.email "email@example.com"
